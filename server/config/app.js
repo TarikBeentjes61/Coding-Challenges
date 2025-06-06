992790b4af740cce964c2c93e878a3e0afffa64f
Tarik <50704992+TarikBeentjes61@users.noreply.github.com>
2025-06-06 11:50:39 +0200

    limiter

--- server/config/app.js
+++ server/config/app.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const { rateLimit } = require("express-rate-limit");
 const cors = require('cors');
 const app = express();
 const challengeRoutes = require('../routes/ChallengeRouter');
 const userRoutes = require('../routes/UserRouter');
 const uploadRoutes = require('../routes/UploadRouter');
 const errorHandler = require('../middleware/errorHandler');
+
+const limiter = {
+    windowMs: 10 * 1000,
+    limit: 5,
+    standardHeaders: true
+}
 
 app.use(cors({ origin: 'http://localhost:3000' }));
 app.use(express.json());
